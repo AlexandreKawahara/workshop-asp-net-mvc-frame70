ec58686b34a3d7af1b0724bd1837347ea69639d5
Alexandre Kawahara <akawahara2@terra.com.br>
2024-09-09 20:21:41 -0300

    SellersController

---
+++ SalesWebMvcFrame70/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvcFrame70.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
