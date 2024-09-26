d611522b78233dadddbaaeb1d128e045dc2d01c1
Alexandre Kawahara <akawahara2@terra.com.br>
2024-09-26 20:36:49 -0300

    Validation

--- SalesWebMvcFrame70/Models/Seller.cs
+++ SalesWebMvcFrame70/Models/Seller.cs
@@ -8,18 +8,26 @@ namespace SalesWebMvcFrame70.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [Display(Name = "E-mail")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 5000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public Double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
