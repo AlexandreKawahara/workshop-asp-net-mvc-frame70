cc02a9c58a1d16748e24bc1b094f44d2a09d80b4
Alexandre Kawahara <akawahara2@terra.com.br>
2024-10-02 19:13:48 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvcFrame70/Controllers/SalesRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvcFrame70.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
